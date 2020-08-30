c1900451ae7a6670443f5ebedae3efe6b5454fd3
Marko Tribl <triblmarko@gmail.com>
2020-08-30 15:34:32 +0200

    Create a class TimedMultiplayerVC' to manage TimedMultiplayerGame

---
+++ VCCompositionWithStoryboards/VCCompositionWithStoryboards/TimedMultiplayerVC.swift
@@ -0,0 +1,15 @@
+//
+//  TimedMultiplayerVC.swift
+//  VCCompositionWithStoryboards
+//
+//  Created by Marko Tribl on 8/30/20.
+//  Copyright © 2020 Marko Tribl. All rights reserved.
+//
+
+import UIKit
+
+class TimedMultiplayerVC: UIViewController {
+    @IBOutlet private weak var timeBar: TimeBarViewController?
+    @IBOutlet private weak var playerOne: PlayerScoreViewController?
+    @IBOutlet private weak var playerTwo: PlayerScoreViewController?
+}
